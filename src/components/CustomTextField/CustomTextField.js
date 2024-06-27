ea50b312142f6478312caaa0469d70550df71430
Rushi G <rushi_g@tekditechnologies.com>
2024-06-27 19:57:03 +0530

    android app icon added

--- src/components/CustomTextField/CustomTextField.js
+++ src/components/CustomTextField/CustomTextField.js
@@ -17,43 +17,6 @@ const CustomTextField= ({text=" Hello World ", position="static", onChangeText, 
     </View>
   );
 }
-<<<<<<< HEAD
-CustomTextField.propTypes = {
-  text: PropTypes.string,
-  position: PropTypes.string,
-};
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    alignItems: 'flex-start',
-    padding: 20,
-    backgroundColor: 'white',
-  },
-  input: {
-    backgroundColor: 'white',
-    width: '100%',
-    height: 65,
-    borderRadius: 7,
-    borderColor: '#DADADA',
-    borderWidth: 1.4,
-    color: 'black',
-    paddingLeft: 20,
-    fontSize: 20,
-    fontFamily: 'Poppins-Regular',
-  },
-  overlap: {
-    top: -76,
-    left: 13,
-    backgroundColor: 'white',
-  },
-  text: {
-    color: '#4D4639',
-    paddingLeft: 2,
-    fontFamily: 'Poppins-Regular',
-    paddingRight: 2,
-  },
-});
-=======
 export default CustomTextField
 const styles =StyleSheet.create({
     container: {
@@ -90,4 +53,3 @@ const styles =StyleSheet.create({
      paddingRight: 2  
     }
 })
->>>>>>> upstream/feat-auth
