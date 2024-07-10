90ab465daae00370cacaaff9b2e768a336f4f632
Rushi G <rushi_g@tekditechnologies.com>
2024-07-10 21:08:29 +0530

    quml player online

--- src/screens/PlayerScreen/QuMLPlayer.js
+++ src/screens/PlayerScreen/QuMLPlayer.js
@@ -1,40 +1,79 @@
-import { View, TouchableOpacity, Image, StyleSheet, Text } from 'react-native';
-import backIcon from '../../assets/images/png/arrow-back-outline.png';
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import CustomButton2 from '../../components/CustomButtonOutlined/CustomButtonOutlined';
-import CustomButton from '../../components/CustomButton/CustomButton';
-import { useTranslation } from '../../context/LanguageContext';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Text,
+  Button,
+} from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
 
 import Logo from '../../assets/images/png/logo.png';
 
 import { WebView } from 'react-native-webview';
 import { Platform } from 'react-native';
+import { playerConfig } from './data';
 
 const QuMLPlayer = () => {
-  const handleWebViewMessage = (event) => {
-    const message = event.nativeEvent.data;
-    console.log('WebView message:', message);
+  const questionListUrl = 'https://sunbirdsaas.com/api/question/v1/list';
+  const jsonObject = {
+    playerConfig: playerConfig,
+    questionListUrl: questionListUrl,
   };
-
   // Determine the correct path to the index.html file based on the platform
   const htmlFilePath = Platform.select({
     ios: './assets/assets/quml/index.html',
     android: 'file:///android_asset/quml/index.html',
+    //android: 'http://192.168.31.74:3000/',
   });
+
+  //set data from react native
+  const webviewRef = useRef(null);
+  const saveJavaScript = `
+      (function() {
+          localStorage.setItem('jsonObject', JSON.stringify(${JSON.stringify(
+            jsonObject
+          )}));
+          return true;
+      })();
+  `;
+  const [retrievedData, setRetrievedData] = useState(null);
+
+  // JavaScript to retrieve localStorage data and send it back to React Native
+  const retrieveJavaScript = `
+      (function() {
+          const data = localStorage.getItem('telemetry');
+          window.ReactNativeWebView.postMessage(data);
+      })();
+  `;
+  const handleMessage = (event) => {
+    const data = event.nativeEvent.data;
+    setRetrievedData(data);
+  };
 
   return (
     <View style={styles.container}>
       <WebView
+        ref={webviewRef}
         originWhitelist={['*']}
         source={Platform.OS === 'ios' ? htmlFilePath : { uri: htmlFilePath }}
         style={styles.webview}
         javaScriptEnabled={true}
         domStorageEnabled={true}
-        onMessage={handleWebViewMessage}
         scalesPageToFit={true}
         startInLoadingState={true}
+        injectedJavaScript={saveJavaScript}
+        onMessage={handleMessage}
       />
+      <Button
+        title="Retrieve telemetry Data"
+        onPress={() => {
+          if (webviewRef.current) {
+            webviewRef.current.injectJavaScript(retrieveJavaScript);
+          }
+        }}
+      />
+      {retrievedData && <Text>{retrievedData}</Text>}
     </View>
   );
 };
