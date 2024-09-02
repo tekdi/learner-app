4eca73ee88e77dd1638d3da7a80c8ef3cfb73a2e
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2024-09-02 18:35:21 +0530

    Download component and alert.

--- src/screens/Dashboard/ContentList.js
+++ src/screens/Dashboard/ContentList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
+  FlatList,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -12,6 +13,8 @@ import TextField from '../../components/TextField/TextField';
 import { courseDetails } from '../../utils/API/ApiCalls';
 import { useNavigation } from '@react-navigation/native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { ProgressBar } from '@ui-kitten/components';
+import globalStyles from '../../utils/Helper/Style';
 
 const ContentList = ({ route }) => {
   const { do_id } = route.params;
@@ -21,6 +24,7 @@ const ContentList = ({ route }) => {
   useEffect(() => {
     const fetchData = async () => {
       const content_do_id = do_id;
+      console.log({ content_do_id });
       const data = await courseDetails(content_do_id);
       console.log();
       setCourses(data?.result?.content?.children?.[0]?.children);
@@ -53,35 +57,43 @@ const ContentList = ({ route }) => {
 
     return null; // or any default value you want to return if no conditions are met
   }
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => {
+        handlePress(item);
+      }}
+    >
+      <View style={styles.view}>
+        <View style={globalStyles.flexrow}>
+          <MaterialIcons
+            name={checkArchiveType(item?.mimeType)}
+            size={32}
+            color="#9cb9ff"
+          />
+          <View style={{ marginLeft: 20, width: '80%' }}>
+            <Text style={styles.text}>
+              <TextField text={item?.name} />(
+              <TextField text={item?.mimeType} />)
+            </Text>
+          </View>
+        </View>
+        <ProgressBar style={{ marginTop: 15 }} progress={0.3} width={'100%'} />
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <SafeAreaView style={{ flex: 1, top: 50 }}>
+    <SafeAreaView style={{ flex: 1, paddingTop: 50 }}>
       {/* <Header /> */}
       <View style={{ padding: 20 }}>
         <TextField style={styles.text} text={'course_details'} />
       </View>
-      {courses?.map((item, index) => {
-        return (
-          <TouchableOpacity
-            onPress={() => {
-              handlePress(item);
-            }}
-          >
-            <View style={styles.view}>
-              <MaterialIcons
-                name={checkArchiveType(item?.mimeType)}
-                size={32}
-                color="#9cb9ff"
-              />
-              <View style={{ marginLeft: 20 }}>
-                <Text style={styles.text}>
-                  <TextField text={item?.name} />(
-                  <TextField text={item?.mimeType} />)
-                </Text>
-              </View>
-            </View>
-          </TouchableOpacity>
-        );
-      })}
+      <FlatList
+        data={courses}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
@@ -97,8 +109,6 @@ styles = StyleSheet.create({
     padding: 20,
     margin: 10,
     borderRadius: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
     backgroundColor: 'white',
     borderColor: '#D0C5B4',
   },
